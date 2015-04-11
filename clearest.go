48dd138fd7bb0bc127023415f00e3c55aee3305f
Esdras Beleza <github@contato.esdrasbeleza.com>
2015-04-11 18:47:57 -0300

    Merge images and get the lightest pixels between them

--- clearest.go
+++ clearest.go
@@ -1,6 +1,9 @@
 package blzimg
 
+import "image"
+import "image/draw"
 import "image/color"
+import "errors"
 
 type ClearestOperation struct{}
 
@@ -14,6 +17,37 @@ func (c ClearestOperation) Clearest(colors []color.RGBA) color.Color {
 	}
 
 	return clearest
+}
+
+func (c ClearestOperation) Result(images []image.Image) (image.Image, error) {
+	firstImage := images[0]
+	bounds := firstImage.Bounds()
+	clearest := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+	draw.Draw(clearest, bounds, firstImage, bounds.Min, draw.Src)
+
+	for _, currentImage := range images {
+		if currentImage.Bounds() != bounds {
+			return nil, errors.New("The images have different size!")
+		}
+
+		imageToCompare := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+		draw.Draw(imageToCompare, bounds, currentImage, bounds.Min, draw.Src)
+		c.getClearestImageBetweenTwo(clearest, imageToCompare)
+	}
+
+	return clearest, nil
+}
+
+func (c ClearestOperation) getClearestImageBetweenTwo(current, other *image.RGBA) {
+	for i := current.Bounds().Min.X; i < current.Bounds().Max.X; i++ {
+		for j := current.Bounds().Min.Y; j < current.Bounds().Max.Y; j++ {
+			currentClearestImagePixel := current.At(i, j).(color.RGBA)
+			otherImagePixel := other.At(i, j).(color.RGBA)
+
+			clearestColor := c.Clearest([]color.RGBA{currentClearestImagePixel, otherImagePixel})
+			current.Set(i, j, clearestColor)
+		}
+	}
 }
 
 func average(color color.Color) uint32 {
--- clearest_test.go
+++ clearest_test.go
@@ -2,11 +2,13 @@ package blzimg
 
 import (
 	"github.com/stretchr/testify/assert"
+	"image"
 	"image/color"
+	"image/draw"
 	"testing"
 )
 
-func TestClearest(t *testing.T) {
+func TestIfWeGetTheRightClearestColorBetweenSomeOfThem(t *testing.T) {
 	operation := ClearestOperation{}
 
 	pixel1 := color.RGBA{128, 22, 33, 0}
@@ -23,4 +25,52 @@ func TestClearestWithEmptyArray(t *testing.T) {
 	black := color.RGBA{0, 0, 0, 0}
 
 	assert.Equal(t, black, operation.Clearest([]color.RGBA{}), "Return black for an empty array")
+}
+
+func TestIfWeGetAImageMadeWithTheClearestPixelsIfWeMergeSomeImages(t *testing.T) {
+	operation := ClearestOperation{}
+
+	black := color.RGBA{0, 0, 0, 0}
+	white := color.RGBA{255, 255, 255, 0}
+
+	/*
+	 * wbb
+	 * wbb
+	 * wbb
+	 */
+	image1 := image.NewRGBA(image.Rect(0, 0, 3, 3))
+	draw.Draw(image1, image1.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
+	image1.Set(0, 0, white)
+	image1.Set(0, 1, white)
+	image1.Set(0, 2, white)
+
+	/*
+	 * bwb
+	 * bwb
+	 * bwb
+	 */
+	image2 := image.NewRGBA(image.Rect(0, 0, 3, 3))
+	draw.Draw(image2, image2.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
+	image2.Set(1, 0, white)
+	image2.Set(1, 1, white)
+	image2.Set(1, 2, white)
+
+	/*
+	 * bbw
+	 * bbw
+	 * bbw
+	 */
+	image3 := image.NewRGBA(image.Rect(0, 0, 3, 3))
+	draw.Draw(image3, image3.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
+	image3.Set(2, 0, white)
+	image3.Set(2, 1, white)
+	image3.Set(2, 2, white)
+
+	mergedImage, _ := operation.Result([]image.Image{image1, image2, image3})
+	bounds := mergedImage.Bounds().Canon()
+	for i := bounds.Min.X; i < bounds.Max.X; i++ {
+		for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
+			assert.Equal(t, white, mergedImage.At(i, j), "Pixel must be white!")
+		}
+	}
 }
