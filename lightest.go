9db31665c667986252a0def7f74af83cfcd2beda
Esdras Beleza <esdras@atlantico.com.br>
2015-04-27 19:16:40 -0300

    Using stupid routines that eat all the machine resources

--- lightest.go
+++ lightest.go
@@ -20,36 +20,73 @@ func (c LightestOperation) lightest(color1, color2 color.RGBA) color.Color {
 func (c LightestOperation) Result(images []ImageContainer) (image.Image, error) {
 	if len(images) == 0 {
 		return nil, nil
-	}
+	} else if len(images) == 1 {
+		return images[0].getImage(), nil
+	} else if len(images) == 2 {
+		firstImage := images[0].getImage()
+		bounds := firstImage.Bounds()
+		lightest := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+		draw.Draw(lightest, bounds, firstImage, bounds.Min, draw.Src)
 
-	firstImage := images[0].getImage()
-	bounds := firstImage.Bounds()
-	lightest := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
-	draw.Draw(lightest, bounds, firstImage, bounds.Min, draw.Src)
-
-	for _, currentImageContainer := range images[1:] {
-		currentImage := currentImageContainer.getImage()
-		if currentImage.Bounds() != bounds {
+		secondImage := images[1].getImage()
+		if secondImage.Bounds() != bounds {
 			return nil, errors.New("The images have different size!")
 		}
 
 		imageToCompare := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
-		draw.Draw(imageToCompare, bounds, currentImage, bounds.Min, draw.Src)
+		draw.Draw(imageToCompare, bounds, secondImage, bounds.Min, draw.Src)
+
 		c.getLightestImageBetweenTwo(lightest, imageToCompare)
+
+		return lightest, nil
+
+	} else {
+		type tuple struct {
+			image image.Image
+			err   error
+		}
+
+		imageChan := make(chan tuple)
+		defer close(imageChan)
+
+		go func() {
+			img, err := c.Result(images[:len(images)/2])
+			imageChan <- tuple{img, err}
+		}()
+
+		go func() {
+			img, err := c.Result(images[len(images)/2:])
+			imageChan <- tuple{img, err}
+		}()
+
+		tuple1 := <-imageChan
+		if tuple1.err != nil {
+			return nil, tuple1.err
+		}
+
+		tuple2 := <-imageChan
+		if tuple2.err != nil {
+			return nil, tuple2.err
+		}
+
+		lightest1 := ImageItselfContainer{tuple1.image}
+		lightest2 := ImageItselfContainer{tuple2.image}
+
+		return c.Result([]ImageContainer{lightest1, lightest2})
 	}
 
-	return lightest, nil
+	return nil, nil
 }
 
-func (c LightestOperation) getLightestImageBetweenTwo(current, other *image.RGBA) {
-	for i := current.Bounds().Min.X; i < current.Bounds().Max.X; i++ {
-		for j := current.Bounds().Min.Y; j < current.Bounds().Max.Y; j++ {
-			currentLightestImagePixel := current.At(i, j).(color.RGBA)
-			otherImagePixel := other.At(i, j).(color.RGBA)
+func (c LightestOperation) getLightestImageBetweenTwo(lightestImage, other *image.RGBA) {
+	for i := lightestImage.Bounds().Min.X; i < lightestImage.Bounds().Max.X; i++ {
+		for j := lightestImage.Bounds().Min.Y; j < lightestImage.Bounds().Max.Y; j++ {
+			pixelFromLightestImage := lightestImage.At(i, j).(color.RGBA)
+			pixelFromOtherImage := other.At(i, j).(color.RGBA)
 
-			lightestColor := c.lightest(currentLightestImagePixel, otherImagePixel)
-			if currentLightestImagePixel != lightestColor {
-				current.Set(i, j, lightestColor)
+			lightestColor := c.lightest(pixelFromLightestImage, pixelFromOtherImage)
+			if pixelFromLightestImage != lightestColor {
+				lightestImage.Set(i, j, lightestColor)
 			}
 		}
 	}
